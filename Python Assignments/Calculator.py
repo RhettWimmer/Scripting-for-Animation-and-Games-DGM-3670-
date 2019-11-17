ca3e20ecabab250c348a6222fba395bfbee579a6
Rhett <thedovahbrine@gmail.com>
2019-11-16 18:42:44 -0700

    ugh python is so fricken much better

--- Python Assignments/Calculator.py
+++ Python Assignments/Calculator.py
@@ -1,12 +1,21 @@
 '''
 Enter a list of values for the calculator to solve in "Values"
 Enter a value in to "Power" to calculate the power of "Values"
-Define "userInput" to tell the calculator what function to solve for. '''
-Values = [100, 10, 10]
+Define "userInput" to tell the calculator what function to solve for.
+    1 = Add
+    2 = Subtract
+    3 = Multiply
+    4 = Divide
+    5 = Power
+    6 = Mean
+    7 = Median
+    8 = Mode
+'''
+Values = [1,5,10,100,10,5,10]
 Power = 3
-userInput = 1000
+userInput = 1
 
-# Addition #
+# Addition input 1 #
 def Add(Vals):
     total = 0
     for i in Vals:
@@ -15,7 +24,7 @@ def Add(Vals):
 Add(Values)
 
 
-# Subtraction #
+# Subtraction input 2 #
 def Sub(Vals):
     total = 0
     for i in Vals:
@@ -24,7 +33,7 @@ def Sub(Vals):
 Sub(Values)
 
 
-# Multiplication #
+# Multiplication input 3 #
 def Multi(Vals):
     total = 1
     for i in Vals:
@@ -33,7 +42,7 @@ def Multi(Vals):
 Multi(Values)
 
 
-# Division #
+# Division input 4 #
 def Divi(Vals):
     total = Vals[0]
     for i in Vals[1:]:
@@ -42,53 +51,59 @@ def Divi(Vals):
 Divi(Values)
 
 
-# Power #
+# Power input 5#
 def Pow(Vals, PowerVal):
     import math
+    if userInput == 5:
+        print 'The powers of ' + str(Values) + " to the power of " + str(Power) + ' = '
     for i in Vals:
-        print math.pow(i, PowerVal)
+        if userInput == 5:
+            print math.pow(i, PowerVal)
 Pow(Values, Power)
 
-# Mean #
+# Mean input 6 #
 def Mean(Vals):
     import statistics
     total = Vals
+    if userInput == 6:
+        print 'The mean of ' + str(Values) + ' = '
+        print statistics.mean(total)
     return statistics.mean(total)
 Mean(Values)
 
-# Median #
+# Median input 7 #
 def Median(Vals):
     import statistics
     total = Vals
+    if userInput == 7:
+        print 'The median of ' + str(Values) + ' = '
+        print statistics.median(total)
     return statistics.median(total)
 Median(Values)
 
-# Mode #
+# Mode input 8 #
 def Mode(Vals):
     import statistics
     total = Vals
-    return statistics.mode(total)
+    if userInput == 8:
+        print 'The mode of ' + str(Values) + ' = '
+        print statistics.mode(total)
+        return statistics.mode(total)
 Mode(Values)
 
 # Input Function #
 def calculator(input):
-    if input == 0:
-        print Add(Values)
     if input == 1:
-        print Sub(Values)
+        print 'Adding ' + str(Values) + ' = '
+        print Add(Values)
     if input == 2:
-        print Multi(Values)
+        print 'Subtracting ' + str(Values) + ' = '
+        print Sub(Values)
     if input == 3:
-        print Divi(Values)
+        print 'Multiplying ' + str(Values) + ' = '
+        print Multi(Values)
     if input == 4:
-        Pow(Values, Power)
-    if input == 5:
-        Mean(Values)
-    if input == 6:
-        Median(Values)
-    if input == 7:
-        Mode(Values)
-    else:
-        print ('!!! Must determine a function to run. Refer to Help for user input directory. !!!')
+        print 'Dividing ' + str(Values) + ' = '
+        print Divi(Values)
 calculator(userInput)
 
