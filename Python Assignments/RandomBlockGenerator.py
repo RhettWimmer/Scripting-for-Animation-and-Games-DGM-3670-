f3c5b96e807be1fe0e1e0289d999f811c3367ada
Rhett Wimmer <35277076+RhettWimmer@users.noreply.github.com>
2019-11-30 21:54:38 -0700

    Update RandomBlockGenerator.py

--- Python Assignments/RandomBlockGenerator.py
+++ Python Assignments/RandomBlockGenerator.py
@@ -1,6 +1,6 @@
 ''' This tool scatters objects randomly in the scene.
     User defined parameters: 
-    AmountOfBlocks = How many blocks will be spawned.
+    AmountOfBlocks = How many objects will be spawned.
     MinXVal, MaxXVal = Scale of were blocks can be spawned on X axis
     MinYVal, MaxYVal = Scale of were blocks can be spawned on Y axis
     MinZVal, MaxZVal = Scale of were blocks can be spawned on Z axis'''    
@@ -16,12 +16,13 @@ MaxZVal = 10
 import maya.cmds as mc
 def randItem(amntBlock, minValX, maxValX, minValY, maxValY, minValZ, maxValZ):
     import random
+    sel = mc.ls(sl = True)
     for i in range(amntBlock):
         randX = random.randint(minValX, maxValX)
         randY = random.randint(minValY, maxValY) 
         randZ = random.randint(minValZ, maxValZ)  
         
-        mc.polyCube()
-        mc.move(randX, randY, randZ)
+        mc.duplicate(sel)
+        mc.move(randX, randY, randZ, sel)
     
 randItem(AmountOfBlocks, MinXVal, MaxXVal, MinYVal, MaxYVal, MinZVal, MaxZVal)
