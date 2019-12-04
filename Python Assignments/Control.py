e27ea03be56326f2e0d48f68a4783d5bcd5cac56
Rhett <thedovahbrine@gmail.com>
2019-12-04 02:45:34 -0700

    Working on control editor

--- Python Assignments/Control.py
+++ Python Assignments/Control.py
@@ -1,1 +1,26 @@
 import maya.cmds as mc
+
+UserDefinedColor = 'red'
+
+def colorControl(UserDefinedColor):   
+    
+    # Color converter #
+    if UserDefinedColor == 'red':
+        colCode = 13
+    
+    # Selection #
+    sels = mc.ls(sl = True)
+    
+    # Color changer #
+    for sel in sels:
+        shapes = mc.listRelatives(sel, c = True, shapes = True)
+        
+        for shape in shapes:
+            mc.setAttr('%s.overrideEnabled' % shape, True)
+            mc.setAttr('%s.overrideColor' % shape, colCode)
+
+colorControl(UserDefinedColor)        
+#piv = (mc.xform(sels, q = True, scalePivot = True))        
+#print piv
+        
+        
