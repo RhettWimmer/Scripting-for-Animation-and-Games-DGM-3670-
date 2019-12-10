280e507bc3121654e23789151ff4def5210f3820
Rhett <thedovahbrine@gmail.com>
2019-12-10 10:38:24 -0700

    Added class to randgen

--- Python Assignments/Final/RandomBlockGenerator.py
+++ Python Assignments/Final/RandomBlockGenerator.py
@@ -4,7 +4,7 @@
     MinXVal, MaxXVal = Scale of were blocks can be spawned on X axis
     MinYVal, MaxYVal = Scale of were blocks can be spawned on Y axis
     MinZVal, MaxZVal = Scale of were blocks can be spawned on Z axis'''    
-
+import maya.cmds as mc
 AmountOfBlocks = 10
 MinXVal = -10
 MaxXVal = 10
@@ -13,16 +13,16 @@ MaxYVal = 10
 MinZVal = -10
 MaxZVal = 10
 
-import maya.cmds as mc
-def randItem(amntBlock, minValX, maxValX, minValY, maxValY, minValZ, maxValZ):
-    import random
-    sel = mc.ls(sl = True)
-    for i in range(amntBlock):
-        randX = random.randint(minValX, maxValX)
-        randY = random.randint(minValY, maxValY) 
-        randZ = random.randint(minValZ, maxValZ)  
-        
-        mc.duplicate(sel)
-        mc.move(randX, randY, randZ, sel)
-    
-randItem(AmountOfBlocks, MinXVal, MaxXVal, MinYVal, MaxYVal, MinZVal, MaxZVal)
+class RG():
+    def randItem(self, amntBlock, minValX, maxValX, minValY, maxValY, minValZ, maxValZ):
+        import random
+        sel = mc.ls(sl = True)
+        for i in range(amntBlock):
+            randX = random.randint(minValX, maxValX)
+            randY = random.randint(minValY, maxValY) 
+            randZ = random.randint(minValZ, maxValZ)  
+            
+            mc.duplicate(sel)
+            mc.move(randX, randY, randZ, sel)
+rg = RG()    
+rg.randItem(AmountOfBlocks, MinXVal, MaxXVal, MinYVal, MaxYVal, MinZVal, MaxZVal)
